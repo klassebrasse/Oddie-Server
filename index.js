af4cba7634b9b7dcad740fab2a15ba835fb8de96
Anton Klasson <anton@leadtub.se>
2022-01-25 00:04:42 +0100

    yäkås

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
-const express = require('express');
+import fetch from "node-fetch";
+import express from "express"
+import * as http from "http";
+import {Server} from "socket.io"
+
 const app = express();
-const http = require('http');
 const server = http.createServer(app);
-const {Server} = require("socket.io");
 const io = new Server(server);
 
 let users = [];
 let odds = [];
 
 const addUser = async (id, roomId, username, deviceToken, color) => {
-    let newUser = {id: id, username: username, roomId: roomId, active: true, deviceToken: deviceToken, color: color, status: null, zips: 0}
+    let newUser = {
+        id: id,
+        username: username,
+        roomId: roomId,
+        active: true,
+        deviceToken: deviceToken,
+        color: color,
+        status: null,
+        zips: 0
+    }
     //await users.push(newUser)
     users.push(newUser)
     //return getUsersInRoom(lobby)
@@ -19,6 +30,27 @@ const getUsersInRoom = (lobby) => {
     return users.filter((user) => user.roomId === lobby)
 }
 
+async function sendPushNotification(expoPushToken) {
+    const message = {
+        to: expoPushToken,
+        sound: 'default',
+        title: 'Ny användare kom in i rummet',
+        body: 'Klicka för att se användaren',
+        data: {someData: 'goes here'},
+    };
+
+    await fetch('https://exp.host/--/api/v2/push/send', {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Accept-encoding': 'gzip, deflate',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(message),
+    });
+}
+
+
 io.on('connection', (socket) => {
     console.log('a user connected');
 
@@ -26,15 +58,19 @@ io.on('connection', (socket) => {
         console.log("user disconnected")
     })
 
-    socket.on('join room', async (roomId, username, color, deviceToken) => {
+    socket.on('join room', async (roomId, username, expoPushToken) => {
         await socket.join(roomId);
 
-        await addUser(socket.id, roomId, username)
+        await addUser(socket.id, roomId, username, expoPushToken)
 
         const listToEmit = getUsersInRoom(roomId)
         console.log(users)
 
         io.to(roomId).emit("users", listToEmit);
+        console.log(expoPushToken)
+        await sendPushNotification(expoPushToken)
+
+
     })
 
 });
